f33a37f212621988534860e77cb43aa8e351ffd2
jhony.dias <jhony.dias@smanager.com.br>
2024-01-03 16:12:25 -0300

    Feature: master -> commit inicial

---
+++ src/main/java/com/invest/agregadorinvestimento/AgregadorInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.invest.agregadorinvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestimentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgregadorInvestimentoApplication.class, args);
+    }
+
+}
